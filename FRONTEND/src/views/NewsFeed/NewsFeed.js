62d3e9906739aa104fe106d644234f64363a9c42
prvn-breach <praveenking880@gmail.com>
2020-12-12 01:24:52 +0530

    changes

--- FRONTEND/src/views/NewsFeed/NewsFeed.js
+++ FRONTEND/src/views/NewsFeed/NewsFeed.js
@@ -10,20 +10,91 @@ export default class NewsFeed extends Component {
         return (
             <div className="container mt-5 pt-5" id="news_feed">
                 <div className="row">
-                    <div className="col-lg-7 p-0 mb-3" id="story_column">
-                        <div className="card" style={{height: '120px'}}>
-                            {/* <div class="d-flex justify-content-start">
-                                <div className="flex-row">
-                                    
-                                </div>
-                            </div> */}
-                        </div>
-                    </div>
+
+                    {/* LEFT SIDE POSTS */}
                     <div className="col-lg-7 p-0" id="posts_column">
                         <Posts />
                     </div>
+
+                    {/* RIGHT SIDE SUGGESTIONS */}
                     <div className="col d-none d-lg-block p-5"  id="profile_column">
                         <Suggestions />
+                    </div>                
+                </div>
+
+                <div id="suggestion_row" className="row bg-white mb-5 py-4 border">
+                    <div className="w-100 col-lg-7 mb-2">
+                        <span className="text-muted">Suggestions For You</span>
+                        <a href="#" className="nav-link p-0 float-right">See All</a>
+                    </div>
+                    <div id="suggestion_col" className="col-lg-7 ml-0 pl-3">
+                        <div className="d-flex justify-content-start" style={{overflowX: "auto"}}>
+                            <div className="border rounded p-3 mr-2" style={{height:"185px"}}>
+                                <div className="pl-4 pr-4">
+                                    <img 
+                                        src="https://instagram.fbkk8-3.fna.fbcdn.net/v/t51.2885-19/44884218_345707102882519_2446069589734326272_n.jpg?_nc_ht=instagram.fbkk8-3.fna.fbcdn.net&_nc_ohc=OqojzNR1_M0AX_TLOA8&oh=1d692889fe9d6514afc3d269733727c4&oe=5FFD060F&ig_cache_key=YW5vbnltb3VzX3Byb2ZpbGVfcGlj.2" 
+                                        height="50"
+                                        width="50"
+                                        className="rounded-circle"
+                                    />
+                                </div>
+                            </div>
+
+                            <div className="border rounded p-3 mr-2" style={{height:"185px"}}>
+                                <div className="pl-4 pr-4">
+                                    <img 
+                                        src="https://instagram.fbkk8-3.fna.fbcdn.net/v/t51.2885-19/44884218_345707102882519_2446069589734326272_n.jpg?_nc_ht=instagram.fbkk8-3.fna.fbcdn.net&_nc_ohc=OqojzNR1_M0AX_TLOA8&oh=1d692889fe9d6514afc3d269733727c4&oe=5FFD060F&ig_cache_key=YW5vbnltb3VzX3Byb2ZpbGVfcGlj.2" 
+                                        height="50"
+                                        width="50"
+                                        className="rounded-circle"
+                                    />
+                                </div>
+                            </div>
+
+                            <div className="border rounded p-3 mr-2" style={{height:"185px"}}>
+                                <div className="pl-4 pr-4">
+                                    <img 
+                                        src="https://instagram.fbkk8-3.fna.fbcdn.net/v/t51.2885-19/44884218_345707102882519_2446069589734326272_n.jpg?_nc_ht=instagram.fbkk8-3.fna.fbcdn.net&_nc_ohc=OqojzNR1_M0AX_TLOA8&oh=1d692889fe9d6514afc3d269733727c4&oe=5FFD060F&ig_cache_key=YW5vbnltb3VzX3Byb2ZpbGVfcGlj.2" 
+                                        height="50"
+                                        width="50"
+                                        className="rounded-circle"
+                                    />
+                                </div>
+                            </div>
+
+                            <div className="border rounded p-3 mr-2" style={{height:"185px"}}>
+                                <div className="pl-4 pr-4">
+                                    <img 
+                                        src="https://instagram.fbkk8-3.fna.fbcdn.net/v/t51.2885-19/44884218_345707102882519_2446069589734326272_n.jpg?_nc_ht=instagram.fbkk8-3.fna.fbcdn.net&_nc_ohc=OqojzNR1_M0AX_TLOA8&oh=1d692889fe9d6514afc3d269733727c4&oe=5FFD060F&ig_cache_key=YW5vbnltb3VzX3Byb2ZpbGVfcGlj.2" 
+                                        height="50"
+                                        width="50"
+                                        className="rounded-circle"
+                                    />
+                                </div>
+                            </div>
+
+                            <div className="border rounded p-3 mr-2" style={{height:"185px"}}>
+                                <div className="pl-4 pr-4">
+                                    <img 
+                                        src="https://instagram.fbkk8-3.fna.fbcdn.net/v/t51.2885-19/44884218_345707102882519_2446069589734326272_n.jpg?_nc_ht=instagram.fbkk8-3.fna.fbcdn.net&_nc_ohc=OqojzNR1_M0AX_TLOA8&oh=1d692889fe9d6514afc3d269733727c4&oe=5FFD060F&ig_cache_key=YW5vbnltb3VzX3Byb2ZpbGVfcGlj.2" 
+                                        height="50"
+                                        width="50"
+                                        className="rounded-circle"
+                                    />
+                                </div>
+                            </div>
+
+                            <div className="border rounded p-3 mr-2" style={{height:"185px"}}>
+                                <div className="pl-4 pr-4">
+                                    <img 
+                                        src="https://instagram.fbkk8-3.fna.fbcdn.net/v/t51.2885-19/44884218_345707102882519_2446069589734326272_n.jpg?_nc_ht=instagram.fbkk8-3.fna.fbcdn.net&_nc_ohc=OqojzNR1_M0AX_TLOA8&oh=1d692889fe9d6514afc3d269733727c4&oe=5FFD060F&ig_cache_key=YW5vbnltb3VzX3Byb2ZpbGVfcGlj.2" 
+                                        height="50"
+                                        width="50"
+                                        className="rounded-circle"
+                                    />
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
--- FRONTEND/src/views/NewsFeed/Posts/Posts.js
+++ FRONTEND/src/views/NewsFeed/Posts/Posts.js
@@ -6,10 +6,12 @@ import "./Posts.css"
 
 export default class Posts extends Component {
     render() {
-
-       
         return (
             <div>
+                <div className="mb-5" id="story_column">
+                    <div className="card" style={{height: '120px'}}></div>
+                </div>
+                
                 <Post />
                 <Post />
             </div>
